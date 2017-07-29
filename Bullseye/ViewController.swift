3f2e837c0a16189cea74ddbe7fb07dde292a829b
Andrey Piterov <pfm@live.ru>
2017-07-29 22:55:50 +0300

    add images

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -67,6 +67,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         startNewRound()
         updateLabels()
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        let thumbImageHighlited = UIImage(named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlited, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
